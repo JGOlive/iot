f64e0dc72fba65732fb8a4eba67138927b54e226
José Oliveira <jose.de.oliveira@tecnico.ulisboa.pt>
2023-05-02 11:51:57 +0100

    make rep

---
+++ lab1/test.py
@@ -0,0 +1,10 @@
+x = 10
+if x < 0:
+    x = 0
+    print("Negative changed to zero")
+elif x == 0:
+    print("It is Zero")
+elif x == 1:
+    print("It is One")
+else:
+    print("It is more than One")
