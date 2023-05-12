765b258ed388d56af03d63dcfc842d2985d0e1a0
José Oliveira <jose.de.oliveira@tecnico.ulisboa.pt>
2023-05-12 14:11:45 +0100

    lab4 3

--- lab4/2 – Datasets.py
+++ lab4/2 – Datasets.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 
 
-iris = pd.read_csv("iris.data", names=["s_lenght","s_width","p_length","p_width","class"])
+iris = pd.read_csv("iris.data", names=["s_length","s_width","p_length","p_width","class"])
 
 print(iris["p_width"])
 
-sb.relplot(data=iris,x=iris["s_lenght"],y=iris["s_width"], hue=iris["class"], size=iris["p_width"], kind="scatter")
+sb.relplot(data=iris,x=iris["s_length"],y=iris["s_width"], hue=iris["class"], size=iris["p_width"], kind="scatter")
 plt.show()
--- lab4/3 – Scikit-learn.py
+++ lab4/3 – Scikit-learn.py
@@ -1,5 +1,11 @@
 import numpy as np
 import scipy as sp
+import pandas as pd
 import sklearn
 from sklearn.model_selection import train_test_split
 
+iris = pd.read_csv("iris.data", names=["s_length","s_width","p_length","p_width","class"])
+
+X_train, X_test, y_train, y_test = train_test_split(iris[["s_length","s_width","p_length","p_width"]], iris["class"], test_size=0.2, random_state=42)
+
+print(X_test)
