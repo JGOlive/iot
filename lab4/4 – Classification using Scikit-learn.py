e59ee401b6d867a23a74e674c4c5601c6b4d3145
João Marques <joao.pedro.freitas.marques@tecnico.ulisboa.pt>
2023-05-18 12:30:12 +0100

    marques vscode

--- lab4/4 – Classification using Scikit-learn.py
+++ lab4/4 – Classification using Scikit-learn.py
@@ -11,19 +11,19 @@ from sklearn.svm import LinearSVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, recall_score, precision_score, confusion_matrix
 
-
+#ler ficheiro e adicionar indice
 iris = pd.read_csv("iris.data", names=["s_length","s_width","p_length","p_width","class"])
 
 # x_train são dados de entrada para treino, y_train sao os dados de saída para treino
 # x_test são os dados de entrada para teste, y_teste são os dadso de saída para teste
 # test_size é a razão absoluta da porção dos dados que é utilizada para teste
-# random state é para o valor inicial
-test_size = 0.9
+# random state é para o valor inicial(seed)
+test_size = 0.9  #mudar para 0.1
 random_state=42
 x_train, x_test, y_train, y_test = train_test_split(iris[["s_length","s_width","p_length","p_width"]], iris["class"], test_size=test_size, random_state=random_state)
 
 ### KNeighbors
-# trainar o amigo
+# treinar o amigo
 knn = KNeighborsClassifier()
 knn.fit(x_train, y_train)
 
