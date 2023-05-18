37606e2c0efd97994efc695a5e1a956b1d67fc69
João Marques <joao.pedro.freitas.marques@tecnico.ulisboa.pt>
2023-05-18 18:41:58 +0100

    fim

--- lab4/marques_treino.py
+++ lab4/marques_treino.py
@@ -5,19 +5,24 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import sklearn
+import yellowbrick
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn import svm
 from sklearn.svm import LinearSVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, recall_score, precision_score, confusion_matrix
+from sklearn import preprocessing
+from yellowbrick.classifier import ClassificationReport
 
 sales_data = pd.read_csv("WA_Fn-UseC_-Sales-Win-Loss.csv")
 
 #print(sales_data.head(n=2))
 #print(sales_data.tail(n=2))
 #print(sales_data.dtypes)
-'''  plot
+
+#plot
+'''
 sns.set(style="whitegrid", color_codes=True)
 
 #setting the plot size for all plots
@@ -34,9 +39,90 @@ plt.show()
 '''
 
 #Violin plot
+'''
 sns.set(rc={'figure.figsize':(16.7,13.27)})
 
 sns.violinplot(x="Opportunity Result",y="Client Size By Revenue",
 hue="Opportunity Result", data=sales_data);
 
 plt.show()
+'''
+
+#labelencoder
+'''
+le = preprocessing.LabelEncoder()
+
+#convert the categorical columns into numeric
+encoded_value = le.fit_transform(["paris", "paris", "tokyo","amsterdam"])
+
+print(encoded_value)
+'''
+
+#print("Supplies Subgroup' : ",sales_data['Supplies Subgroup'].unique())
+
+le = preprocessing.LabelEncoder()
+
+#convert the categorical columns into numeric
+sales_data['Supplies Subgroup'] = le.fit_transform(sales_data['Supplies Subgroup'])
+sales_data['Region'] = le.fit_transform(sales_data['Region'])
+sales_data['Route To Market'] = le.fit_transform(sales_data['Route To Market'])
+sales_data['Opportunity Result'] = le.fit_transform(sales_data['Opportunity Result'])
+sales_data['Competitor Type'] = le.fit_transform(sales_data['Competitor Type'])
+sales_data['Supplies Group'] = le.fit_transform(sales_data['Supplies Group'])
+
+
+#display the initial records
+#print(sales_data.head())
+
+
+cols = [col for col in sales_data.columns if col not in ['Opportunity Number','Opportunity Result']]
+
+data = sales_data[cols]
+
+target = sales_data['Opportunity Result']
+
+#print(data.head(n=2))
+
+data_train, data_test, target_train, target_test = train_test_split(data,target, test_size = 0.30, random_state = 10)
+
+
+#AI STUFZ     Naive
+'''
+gnb = GaussianNB()
+
+pred = gnb.fit(data_train, target_train).predict(data_test)
+#print(pred.tolist())
+#print the accuracy score of the model
+print("Naive-Bayes accuracy : ",accuracy_score(target_test, pred,normalize = True))
+
+visualizer = ClassificationReport(gnb, classes=['Won','Loss'])
+visualizer.fit(data_train, target_train) # Fit the training data to the visualizer
+visualizer.score(data_test, target_test) # Evaluate the model on the test data
+g = visualizer.poof() # Draw/show/poof the data
+'''
+
+# LINEAR SVC
+
+svc_model = LinearSVC(random_state=0)
+#train the algorithm on training data and predict using the testing data
+pred = svc_model.fit(data_train, target_train).predict(data_test)
+#print the accuracy score of the model
+print("LinearSVC accuracy : ",accuracy_score(target_test, pred, normalize = True))
+
+#Instantiate the classification model and visualizer
+visualizer = ClassificationReport(svc_model, classes=['Won','Loss'])
+visualizer.fit(data_train, target_train) # Fit the training data to the visualizer
+visualizer.score(data_test, target_test) # Evaluate the model on the test data
+g = visualizer.poof() # Draw/show/poof the data
+
+#k-neighboors
+'''
+#create object of the lassifier
+neigh = KNeighborsClassifier(n_neighbors=3)
+#Train the algorithm
+neigh.fit(data_train, target_train)
+#predict the response
+pred = neigh.predict(data_test)
+# evaluate accuracy
+print ("KNeighbors accuracy score : ",accuracy_score(target_test, pred))
+'''
