fa6be2a67775def5b398594c5c56122604511211
José Oliveira <jose.de.oliveira@tecnico.ulisboa.pt>
2023-05-26 18:24:09 +0100

    changes to TestMe

--- proj1/TestMe.py
+++ proj1/TestMe.py
@@ -1,5 +1,15 @@
+import pandas as pd
 import pickle
+
+df = pd.read_csv("Lab6-Proj1_TestSet.csv")
 
 NN_filename = "NN_Model.pkl"
 with open(NN_filename, "rb") as file:
     nn_model = pickle.load(file)
+
+# remove lines with missing information
+test_file = df.dropna()
+
+
+
+print(test_file)
