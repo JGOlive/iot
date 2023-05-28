fa36ea8727e4cb2ec874fa5bc4721573b143562b
João Marques <joao.pedro.freitas.marques@tecnico.ulisboa.pt>
2023-05-28 16:47:08 +0100

    s

--- proj1/proj1.py
+++ proj1/proj1.py
@@ -32,11 +32,11 @@ df_dataset.dropna(inplace=True)
 
 z_scores_dataset = z_scores[z_scores <= threshold]
 
-'''
+
 # visual representation of the chosen points
 plt.plot(z_scores_dataset) # para cada categoria exemplo: plt.plot(z_scores_dataset["ESLE"])
 plt.show()
-'''
+
 
 # Train, Validation, Test split
 # 70% train 15% validation 15% test
